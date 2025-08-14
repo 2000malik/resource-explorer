5f57b82280fe212b1a7c3f751778c13efcc6b80b
Malik <36223770+2000malik@users.noreply.github.com>
2025-08-14 15:04:05 +0100

    misc: character type casting

---
+++ src/types/global.d.ts
@@ -0,0 +1,41 @@
+type GlobalResponseData<T> = {
+  statusMessage: string;
+  status: number;
+  data: T;
+};
+
+type ErrorResponseData = {
+  statusCode: number;
+  message: string;
+  error?: string;
+};
+
+type PaginationType = {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+};
+
+type CharacterType = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Origin;
+  location: Location;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+type Origin = {
+  name: string;
+  url: string;
+};
+type Location = {
+  name: string;
+  url: string;
+};
