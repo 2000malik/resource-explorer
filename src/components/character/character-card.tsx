442a6805bd1c897f96746f38ebd5a2cbff1a1520
Malik <36223770+2000malik@users.noreply.github.com>
2025-08-14 21:01:16 +0100

    patch: improved character card component

--- src/components/character/character-card.tsx
+++ src/components/character/character-card.tsx
@@ -1,7 +1,10 @@
-import { Card, CardContent, CardMedia, IconButton } from "@mui/material";
-import { useFavorites } from "@/src/store/useFavorite";
-import { Typography } from "../typography";
+import Link from "next/link";
+import { Box, Card, CardContent, CardMedia, IconButton } from "@mui/material";
+// 
 import { colors } from "@/src/theme/color";
+import { useFavorites } from "@/src/store/useFavorite";
+// 
+import { Typography } from "../typography";
 
 export function CharacterCard({ character }: { character: CharacterType }) {
   const { favorites, toggleFavorite } = useFavorites();
@@ -22,20 +25,32 @@ export function CharacterCard({ character }: { character: CharacterType }) {
         <Typography variant="body2" sx={{ color: colors.grey.secondary }}>
           {character.species}
         </Typography>
-        <IconButton
-          onClick={() => toggleFavorite(character.id)}
-          sx={{
-            color: isFav ? colors.primary.main : colors.primary.secondary,
-            fontSize: 14,
-            fontWeight: 600,
-            padding: 0,
-            "&:hover": {
-              background: "transparent",
-            },
-          }}
-        >
-          Add to favorites
-        </IconButton>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <IconButton
+            onClick={() => toggleFavorite(character.id)}
+            sx={{
+              color: isFav ? colors.primary.main : colors.primary.secondary,
+              fontSize: 14,
+              fontWeight: 600,
+              padding: 0,
+              "&:hover": {
+                background: "transparent",
+              },
+            }}
+          >
+            Add to favorites
+          </IconButton>
+          <Link
+            href={`/${character.id}`}
+            style={{
+              color: colors.primary.main,
+              fontSize: 18,
+              fontWeight: 600,
+            }}
+          >
+            {">"}
+          </Link>
+        </Box>
       </CardContent>
     </Card>
   );
