14a7ef94a5d6e31d19d6cae2f9fa3b56a9e22ba3
Malik <36223770+2000malik@users.noreply.github.com>
2025-08-14 19:07:39 +0100

    misc: create character card

---
+++ src/components/character/character-card.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { useFavorites } from "@/src/store/useFavorite";
+
+export default function CharacterCard({
+  character,
+}: {
+  character: CharacterType;
+}) {
+  const { favorites, toggleFavorite } = useFavorites();
+  const isFav = favorites.includes(character.id);
+
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="200"
+        image={character?.image}
+        alt={character?.name}
+      />
+      <CardContent>
+        <Typography variant="h6">{character.name}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {character.species}
+        </Typography>
+        <IconButton
+          onClick={() => toggleFavorite(character.id)}
+          color={isFav ? "error" : "default"}
+        >
+          Add to favorites
+        </IconButton>
+      </CardContent>
+    </Card>
+  );
+}
